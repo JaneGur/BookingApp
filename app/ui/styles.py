1f14f115428932177447586cc74b911bfe7cf0bf
Evgeniya <iliving2007@yandex.ru>
2025-11-08 05:34:01 +0200

    08 11 2025 2th

--- app/ui/styles.py
+++
@@ -1,6 +0,0 @@
-from .styles import load_all_styles, load_public_styles, load_admin_styles
-
-# Для обратной совместимости
-def load_custom_css():
-    """Основная функция загрузки стилей"""
-    load_all_styles()
